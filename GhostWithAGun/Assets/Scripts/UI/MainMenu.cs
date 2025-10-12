f8793ac4aa021d088b94e2ad207996291a97c3a5
GrimWitch76 <grimwitch76@gmail.com>
2025-10-12 08:19:50 -0700

    Prep for main menu functionality

---
+++ GhostWithAGun/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void UI_PlayGame()
+    {
+
+    }
+
+    public void UI_ResetGame()
+    {
+
+    }
+
+    public void UI_OpenReviews()
+    {
+
+    }
+
+    public void UI_OpenSettings()
+    {
+        
+    }
+}
